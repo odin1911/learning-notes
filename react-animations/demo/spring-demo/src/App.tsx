d9b62f32db89766a04efbd23b764ff32c99b830f
min.li <min.li@alo7.com>
2021-12-08 16:43:12 +0800

    update

--- react-animations/demo/spring-demo/src/App.tsx
+++ react-animations/demo/spring-demo/src/App.tsx
@@ -1,15 +1,13 @@
 import React from "react";
 import { Basic1, Basic2 } from "./spring/basics";
 import { BackwardsCompatability } from "./spring/imperatives-and-refs";
-import { TransitionArray } from "./spring/useTransition";
 
 function App() {
   return (
     <>
       {/* <Basic1 />
       <Basic2 /> */}
-      {/* <BackwardsCompatability /> */}
-      <TransitionArray />
+      <BackwardsCompatability />
     </>
   );
 }
